7f742326fe164a08761cf917b7f5a8822149edd3
Georgi Sabev <georgethebeatle@gmail.com>
2022-11-14 10:36:46 +0000

    Replace deprecated method in nvim-cmp setup
    Co-authored-by: Kieron Browne <kbrowne@vmware.com>

--- nvim/.config/nvim/lua/config/lsp.lua
+++ nvim/.config/nvim/lua/config/lsp.lua
@@ -1,7 +1,7 @@
 local nvim_lsp = require 'lspconfig'
 
 -- Setup lspconfig.
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 -- disable inline diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
