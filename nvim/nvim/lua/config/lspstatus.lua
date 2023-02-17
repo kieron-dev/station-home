7844a11d52c3373cc6d55790915c85073e9dbeb5
Kieron Browne <kbrowne@vmware.com>
2023-02-17 14:34:53 +0000

    Get rid of custom status icons
    Keep indicator_hint, as the default doesn't seem to be valid
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@vmware.com>

--- nvim/nvim/lua/config/lspstatus.lua
+++ nvim/nvim/lua/config/lspstatus.lua
@@ -2,11 +2,6 @@ local lsp_status = require 'lsp-status'
 
 lsp_status.config{
     status_symbol = '',
-    current_function = false,
-    indicator_errors = '❌',
-    indicator_warnings = '⚠️ ',
-    indicator_info = 'ℹ️',
-    indicator_hint = '🤔',
-    indicator_ok = '✅',
+    indicator_hint = '',
 }
 lsp_status.register_progress()
