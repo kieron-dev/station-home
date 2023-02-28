5357d5b887c34d7d602e278b76e1a7ac0795820b
Kieron Browne <kbrowne@vmware.com>
2023-02-28 16:00:45 +0000

    Show relative path in lualine

--- nvim/nvim/lua/config/plug.lua
+++ nvim/nvim/lua/config/plug.lua
@@ -25,6 +25,9 @@ require("lazy").setup({
         'nvim-lualine/lualine.nvim',
         opts = {
             theme = "tokyonight",
+            sections = {
+                lualine_c = {{ "filename", path = 1 }},
+            },
         },
     },
     -- Preview markdown files in the browser
