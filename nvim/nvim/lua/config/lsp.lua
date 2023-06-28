443e58544247e3348182354baec5650e78fe4c94
Kieron Browne <kbrowne@vmware.com>
2023-06-28 12:57:08 +0000

    Put border around LSP floats
    It makes diagnostics and docs easier to see
    
    Co-authored-by: Danail Branekov <danailster@gmail.com>

--- nvim/nvim/lua/config/lsp.lua
+++ nvim/nvim/lua/config/lsp.lua
@@ -12,6 +12,24 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     }
 )
 
+local _border = "single"
+
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+  vim.lsp.handlers.hover, {
+    border = _border
+  }
+)
+
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+  vim.lsp.handlers.signature_help, {
+    border = _border
+  }
+)
+
+vim.diagnostic.config{
+  float={border=_border}
+}
+
 vim.fn.sign_define("DiagnosticSignError", {text = " ", texthl = "DiagnosticSignError"})
 vim.fn.sign_define("DiagnosticSignWarn", {text = " ", texthl = "DiagnosticSignWarn"})
 vim.fn.sign_define("DiagnosticSignInfo", {text = " ", texthl = "DiagnosticSignInfo"})
