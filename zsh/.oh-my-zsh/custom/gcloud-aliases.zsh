aa040706d83850466d96761780500ea364f8ae7a
Kieron Browne <kbrowne@vmware.com>
2023-07-27 13:32:16 +0000

    tap-sandbox fixes

--- zsh/.oh-my-zsh/custom/gcloud-aliases.zsh
+++ zsh/.oh-my-zsh/custom/gcloud-aliases.zsh
@@ -1,3 +1,4 @@
 alias gcloudlogin='gcloud auth login'
 alias gcloudclustereirini='gcloud container clusters get-credentials --zone europe-west1-b --project cff-eirini-peace-pods'
-alias gcloudcluster='gcloud container clusters get-credentials --zone europe-west1-b --project cf-on-k8s-wg'
+alias gcloudclustercfonk8s='gcloud container clusters get-credentials --zone europe-west1-b --project cf-on-k8s-wg'
+alias gcloudcluster='gcloud container clusters get-credentials --zone us-central1-a --project tap-sandbox-dev'
