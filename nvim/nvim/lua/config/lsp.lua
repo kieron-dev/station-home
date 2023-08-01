8fa69256d3301de684711e71acb4b7cb748f0549
Kieron Browne <kbrowne@vmware.com>
2023-08-01 16:02:09 +0100

    pyright lsp

--- nvim/nvim/lua/config/lsp.lua
+++ nvim/nvim/lua/config/lsp.lua
@@ -91,7 +91,7 @@ end
 
 -- Use a loop to conveniently both setup defined servers
 -- and map buffer local keybindings when the language server attaches
-local servers = { "tsserver", "bashls" }
+local servers = { "tsserver", "bashls", "pyright" }
 for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
         on_attach = on_attach,
